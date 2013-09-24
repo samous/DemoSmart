2f5f577ae514ec85fcd4cf4b6c1c46174f158ea3
samous <samuel.kerboeuf@gmail.com>
2013-09-24 11:07:39 +0200

    Adding ViewController

---
+++ DemoSmart/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DemoSmart
+//
+//  Created by Samuel on 24/09/13.
+//  Copyright (c) 2013 Mobvalue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
