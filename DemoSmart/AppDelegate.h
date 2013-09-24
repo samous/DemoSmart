75ec9b9a6720fa08546b380af3a128f58cd6aa12
samous <samuel.kerboeuf@gmail.com>
2013-09-24 10:58:45 +0200

    Initial Commit

---
+++ DemoSmart/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoSmart
+//
+//  Created by Samuel on 24/09/13.
+//  Copyright (c) 2013 Mobvalue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DemoSmartTests/DemoSmartTests.m
@@ -0,0 +1,34 @@
+//
+//  DemoSmartTests.m
+//  DemoSmartTests
+//
+//  Created by Samuel on 24/09/13.
+//  Copyright (c) 2013 Mobvalue. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DemoSmartTests : XCTestCase
+
+@end
+
+@implementation DemoSmartTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
